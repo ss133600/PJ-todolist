a0403e0e11b6bbec908f8c657ca09b4b0afd2478
ss133600 <S9878133600@gmail.com>
2024-03-15 01:54:04 +0900

    프로젝트 준비 및 TodoTemplate 제작

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoWrapper = styled.div`
+  width: 512px;
+  margin: 6rem auto;
+  border-radius: 4px;
+  overflow: hidden;
+  box-shadow: 0 0 5px #ccc;
+`;
+
+const AppTitle = styled.h1`
+  background-color: #22b8cf;
+  font-size: 16px;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  font-weight: 400;
+`;
+
+const Content = styled.div`
+  background-color: #fff;
+`;
+
+const TodoTemplate = () => {
+  return (
+    <TodoWrapper>
+      <AppTitle>Todo List</AppTitle>
+      <Content>content</Content>
+    </TodoWrapper>
+  );
+};
+
+export default TodoTemplate;
